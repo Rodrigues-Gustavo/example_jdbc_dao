808a4d7f349f9d4050040628d9ffc65d28bb2061
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-11-11 10:53:26 -0300

    seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department d = new Department(1, "sneakers");
-		System.out.println(d);
+		
+		Seller seller = new Seller(22, "javaboy", "javaboy@gmail.com", new Date(), 10000.0, d);
+		System.out.println(seller);
 	}
 }
